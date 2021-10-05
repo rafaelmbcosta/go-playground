a9fccbe65bf3ad6b736e7901f954c86fba0aa0aa
Rafael Costa <rafaelmbcosta@gmail.com>
2021-10-04 23:48:48 -0300

    adding the webservice

---
+++ go-web-service/handlers/handlers.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"golang-fifa-world-cup-web-service/data"
+	"net/http"
+)
+
+// RootHandler returns an empty body status code
+func RootHandler(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(http.StatusNoContent)
+}
+
+// ListWinners returns winners from the list
+func ListWinners(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	year := req.URL.Query().Get()
+
+	winners, err := data.ListAllJSON()
+
+	if err == nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res.Write(winners)
+}
+
+// AddNewWinner adds new winner to the list
+func AddNewWinner(res http.ResponseWriter, req *http.Request) {
+
+}
+
+// WinnersHandler is the dispatcher for all /winners URL
+func WinnersHandler(res http.ResponseWriter, req *http.Request) {
+
+}
