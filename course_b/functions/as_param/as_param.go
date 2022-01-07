cf6a02a632927dc27b2dd87ad2f401a3e119568f
Rafael Costa <rafaelmbcosta@gmail.com>
2022-01-07 20:00:07 -0300

    Quick example for demonstrate GIT

--- course_b/functions/as_param/as_param.go
+++ course_b/functions/as_param/as_param.go
@@ -6,11 +6,16 @@ func multiply(a, b int) int {
 	return a * b
 }
 
+func sum(a, b int) int {
+	return a + b
+}
+
 func exec(function func(int, int) int, p1, p2 int) int {
 	return function(p1, p2)
 }
 
 func main() {
+	fmt.Println("SOMA =", sum(3, 4))
 	result := exec(multiply, 3, 4)
 	fmt.Println(result)
 }
